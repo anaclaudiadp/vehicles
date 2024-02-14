536e9337934157eef6c8ce6d08d7b2647993cc4b
Paul Jervis <jervis@12os.co.uk>
2024-02-14 20:16:57 +0000

    question 5

--- anapaul_python/main.py
+++ anapaul_python/main.py
@@ -1,6 +1,5 @@
 from functools import reduce
 
-
 if __name__ == '__main__':
     left = open('../left.csv', 'r')
     distances = {}
@@ -14,11 +13,13 @@ if __name__ == '__main__':
     for k, v in distances.items():
         print(k, v)
 
+    # Question 2
     print("\n\nTotal of all vehicles:")
     print(sum(distances.values()))
     print(reduce(lambda acc, each: acc + each, distances.values()))
-    # this reducer is adding the values together one at a time, then returning the the total - the same as sum.
 
+
+    # this reducer is adding the values together one at a time, then returning the the total - the same as sum.
 
     def function(top, each):
         if top is None:
@@ -35,6 +36,35 @@ if __name__ == '__main__':
     maximum2 = reduce(lambda top, each: each if top[1] < each[1] else top, distances.items())
     # maximum using a lambda and a ternary statement
 
+    # Question 3
     print(maximum1)
     print(maximum2)
 
+    # Question 5
+    left = open('../left.csv', 'r')
+
+    vehicle_names = list(distances.keys())
+    passing = dict()
+    v1 = vehicle_names[0]
+    v2 = vehicle_names[1]
+    passing[v1] = 0
+    passing[v2] = 0
+
+    times_passed = 0
+
+    for line in left.readlines():
+        data = line.strip('\n').split(',')
+        vehicle_id = data[1]
+
+        if vehicle_id not in passing:
+            continue
+
+        current_distance = passing[vehicle_id] + int(data[2])
+
+        if vehicle_id == v1 and passing[v1] < passing[v2] < current_distance:
+            times_passed = times_passed + 1
+
+        passing[vehicle_id] = current_distance
+
+    # Question 5
+    print("Vehicle 1 passed Vehicle 2 %d times" % times_passed)
